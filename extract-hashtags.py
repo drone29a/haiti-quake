74dd8f420c25df786ab301a0f8f07d4a6bf98662
Joey Harrison <joey.f.harrison@gmail.com>
2010-04-24 14:41:10 -0400

    Added a file.

---
+++ extract-hashtags.py
@@ -0,0 +1,36 @@
+##
+#
+# Read in a .csv file containing twitter data and output the hashtags and the
+# tweet IDs under which they were tweeted.
+#
+##
+
+import sys, csv, re
+
+def print_usage():
+    print "python extract-hashtags.py <filename>"
+
+def main(argv=None):
+	argv = sys.argv if argv is None else argv
+    
+	if len(argv) < 1:
+		print_usage()
+		return
+		
+	filename = argv[1]
+	reader = csv.reader(open(filename), delimiter=',', quotechar='"')
+	
+	headers = reader.next()
+	d = dict()
+	for index, item in enumerate(headers):
+		d[item] = index
+	
+	hash_regex = re.compile(r'#[0-9a-zA-Z+_]*',re.IGNORECASE)
+	user_regex = re.compile(r'@[0-9a-zA-Z+_]*',re.IGNORECASE)
+	
+	for row in reader:
+		for ht in hash_regex.findall(row[d['text']]):
+			print row[d['id']]+',', ht.lower()
+
+if __name__ == '__main__':
+	sys.exit(main())
